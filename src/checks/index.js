0fd8d255d7226b926e228ae0b12aeef164910e3e
Eddie Jaoude <eddie@jaoudestudios.com>
2024-10-01 13:00:37 +0100

    feat: check files added

---
+++ src/checks/index.js
@@ -0,0 +1,77 @@
+import activity from "./activity";
+import branches from "./branches";
+import codeOfConduct from "./codeOfConduct";
+import contributing from "./contributing";
+import defaultBranch from "./defaultBranch";
+import description from "./description";
+import goodFirstIssue from "./goodFirstIssue";
+import issues from "./issues";
+import issueTemplates from "./issueTemplates";
+import labels from "./labels";
+import license from "./license";
+import projects from "./projects";
+import pullRequestTemplate from "./pullRequestTemplate";
+import readme from "./readme";
+import release from "./release";
+import topics from "./topics";
+import url from "./url";
+
+/*
+ * Every check must return the following format
+ * {
+ *   id: "activity",            // unique id (use "-" if needed)
+ *   href: "/repo/status",      // link to more info
+ *   title: "Activity",         // display title
+ *   status: "warning",         // success/warning/error
+ *   description: "...",        // more info about the check
+ *   extra: "...",              // suggestion on next steps
+ * }
+ */
+
+export default function checks(data, ignoreChecks = []) {
+  const allChecks = [
+    description(data.repo),
+    url(data.repo),
+    topics(data.repo),
+    activity(data.repo),
+    issues(data.repo),
+    defaultBranch(data.repo),
+    goodFirstIssue(data.issues),
+    branches(data.branches),
+    release(data.release),
+    readme(data.communityMetrics),
+    license(data.communityMetrics),
+    contributing(data.communityMetrics),
+    // issueTemplates(data.communityMetrics), // TODO data from github json is always null
+    pullRequestTemplate(data.communityMetrics),
+    codeOfConduct(data.communityMetrics),
+    labels(data.labels),
+    projects(data.repo, data.projects),
+  ];
+
+  const userChecks = filterIgnoredChecks(allChecks, ignoreChecks);
+
+  const summary = checkSummary(userChecks);
+
+  return { checks: userChecks, summary, allChecks, ignoreChecks };
+}
+
+export function checkSummary(checks) {
+  return Object.groupBy(checks, ({ status }) => status);
+}
+
+export function worstCheck(
+  check,
+  error = "error",
+  warning = "warning",
+  success = "success",
+) {
+  if (!check) {
+    return "unknow";
+  }
+  return check.red > 0 ? error : check.amber > 0 ? warning : success;
+}
+
+export function filterIgnoredChecks(checks, ignoredChecks) {
+  return checks.filter((check) => !ignoredChecks.includes(check.id));
+}
